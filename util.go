fc4a304a8503ce8c0b0397b895bfb50c7030c5f3
Aaron Wang <aaron.wang@datadoghq.com>
2024-03-13 10:29:47 -0400

    pushing dylan's changes to slice ops

--- util.go
+++ util.go
@@ -3,10 +3,7 @@ package gorocksdb
 // #include <stdlib.h>
 
 import "C"
-import (
-	"reflect"
-	"unsafe"
-)
+import "unsafe"
 
 // btoi converts a bool value to int.
 func btoi(b bool) int {
@@ -24,12 +21,14 @@ func boolToChar(b bool) C.uchar {
 	return 0
 }
 
+// charToBool converts C.uchar to bool value
+func charToBool(c C.uchar) bool {
+	return c != 0
+}
+
 // charToByte converts a *C.char to a byte slice.
 func charToByte(data *C.char, len C.size_t) []byte {
-	var value []byte
-	sH := (*reflect.SliceHeader)(unsafe.Pointer(&value))
-	sH.Cap, sH.Len, sH.Data = int(len), int(len), uintptr(unsafe.Pointer(data))
-	return value
+	return unsafe.Slice((*byte)(unsafe.Pointer(data)), int(len))
 }
 
 // byteToChar returns *C.char from byte slice.
@@ -53,22 +52,15 @@ func cByteSlice(b []byte) *C.char {
 
 // stringToChar returns *C.char from string.
 func stringToChar(s string) *C.char {
-	ptrStr := (*reflect.StringHeader)(unsafe.Pointer(&s))
-	return (*C.char)(unsafe.Pointer(ptrStr.Data))
+	return (*C.char)(unsafe.Pointer(unsafe.StringData(s)))
 }
 
 // charSlice converts a C array of *char to a []*C.char.
 func charSlice(data **C.char, len C.int) []*C.char {
-	var value []*C.char
-	sH := (*reflect.SliceHeader)(unsafe.Pointer(&value))
-	sH.Cap, sH.Len, sH.Data = int(len), int(len), uintptr(unsafe.Pointer(data))
-	return value
+	return unsafe.Slice(data, int(len))
 }
 
 // sizeSlice converts a C array of size_t to a []C.size_t.
 func sizeSlice(data *C.size_t, len C.int) []C.size_t {
-	var value []C.size_t
-	sH := (*reflect.SliceHeader)(unsafe.Pointer(&value))
-	sH.Cap, sH.Len, sH.Data = int(len), int(len), uintptr(unsafe.Pointer(data))
-	return value
+	return unsafe.Slice(data, int(len))
 }
